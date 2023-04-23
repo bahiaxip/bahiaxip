d800d4d5a7347d8e943a86791264f90be981ab86
Xip <bahiaxip@hotmail.com>
2023-04-23 22:16:36 +0200

    update styles CSS in projects view

--- public/js/animation_gsap.js
+++ public/js/animation_gsap.js
@@ -435,7 +435,13 @@ window.addEventListener('load',()=>{
         animationFooter('contact',null,3.5)
     }
     if(route == 'projects'){
-        gsap.to('.div_projects',{opacity:1,duration:1,delay:2,ease:'power4.out'})
+        gsap.to('.projects',{
+            opacity:1,
+            duration:2,
+            delay:2.5,
+            stagger:0.5
+            
+        })
     }
     
 
